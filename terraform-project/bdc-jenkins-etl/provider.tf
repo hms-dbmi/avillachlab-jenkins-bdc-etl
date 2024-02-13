913e12ea2e801138d54c6786272c300c0ae5217b
Thomas DeSain <Thomas_DeSain@hms.harvard.edu>
2024-02-13 15:19:34 -0500

    Going to stick with terra v.12 for now. # Want to look further into genomic infrastructure ( terraform work ) with Sharra.  We will need to get that updated to modern terraform versions.

--- terraform-project/bdc-jenkins-etl/provider.tf
+++ terraform-project/bdc-jenkins-etl/provider.tf
@@ -1,8 +1,4 @@
-terraform {
-  required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = "~> 5.0"
-    }
-  }
+provider "aws" {
+  region     = "us-east-1" 
+  version    = "3.74"
 }
