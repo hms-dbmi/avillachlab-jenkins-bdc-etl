91335734a15ee75d2ec91958d5f4dc8453be3f4c
Thomas DeSain <Thomas_DeSain@hms.harvard.edu>
2024-03-06 14:11:54 -0500

    removing http and https ingress.  We do not use them as we do ssh port forwarding.

--- terraform-project/modules/bdc-jenkins-etl/main.tf
+++ terraform-project/modules/bdc-jenkins-etl/main.tf
@@ -64,26 +64,6 @@ resource "aws_security_group" "inbound" {
   description = "Allow inbound traffic from private network on ports 22, 80 and 443"
   vpc_id      = var.vpc_id
 
-  # need to d
-  ingress {
-    from_port = 80
-    to_port   = 80
-    protocol  = "tcp"
-    cidr_blocks = [
-      var.access_cidr
-    ]
-  }
-
-  ingress {
-    from_port = 443
-    to_port   = 443
-    protocol  = "tcp"
-    cidr_blocks = [
-      var.access_cidr
-
-    ]
-  }
-
   ingress {
     from_port = 22
     to_port   = 22
